4a74659f2f16b5c9c393aa85ae3807904cef85b4
Breno Costa <brenocostanascimento031@gmail.com>
2026-08-17 14:06:39 -0300

    fix: notblank cannot be used with long type

--- src/main/java/com/brenocosta/helpdeskapi/dtos/user/UpdateRoleDTO.java
+++ src/main/java/com/brenocosta/helpdeskapi/dtos/user/UpdateRoleDTO.java
@@ -1,6 +1,7 @@
 package com.brenocosta.helpdeskapi.dtos.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record UpdateRoleDTO(@NotBlank Long roleId) {
+public record UpdateRoleDTO(@NotNull Long roleId) {
 }
